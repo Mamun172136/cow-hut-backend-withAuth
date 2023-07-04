e49fbd1d2dc73f75284b5166b2dd6e040bf698c2
Mamun172136 <ma1536204@gmail.com>
2023-07-04 14:28:52 +0600

    add  refresh token  zodscema

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -11,13 +11,13 @@ const loginZodSchema = z.object({
   }),
 })
 
-// const refreshTokenZodSchema = z.object({
-//   cookies: z.object({
-//     refreshToken: z.string({
-//       required_error: 'Refresh Token is required',
-//     }),
-//   }),
-// })
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh Token is required',
+    }),
+  }),
+})
 
 export const AuthValidation = {
   loginZodSchema,
