1e4806261109022438fd9b6ccc7b8ad8aef24cfb
Mamun172136 <ma1536204@gmail.com>
2023-07-04 14:30:50 +0600

    add auth middleware

--- src/app/middlewres/auth.ts
+++ src/app/middlewres/auth.ts
@@ -1,38 +1,38 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { NextFunction, Request, Response } from 'express'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express'
 
-// import ApiError from '../../errors/ApiError'
-// import jwt from 'jsonwebtoken'
+import ApiError from '../../errors/ApiError'
+import jwt from 'jsonwebtoken'
 
-// const auth =
-//   (...requiredRoles: string[]) =>
-//   async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       //get authorization token
-//       const token = req.headers.authorization
-//       if (!token) {
-//         throw new ApiError(401, 'You are not authorized')
-//       }
-//       // verify token
-//       let verifiedToken = null
-//       try {
-//         verifiedToken = jwt.verify(token, 'secret')
-//         console.log('decoded:', verifiedToken)
-//       } catch (error) {
-//         throw new ApiError(403, 'invalid  token')
-//       }
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.headers.authorization
+      if (!token) {
+        throw new ApiError(401, 'You are not authorized')
+      }
+      // verify token
+      let verifiedToken = null
+      try {
+        verifiedToken = jwt.verify(token, 'secret')
+        console.log('decoded:', verifiedToken)
+      } catch (error) {
+        throw new ApiError(403, 'invalid  token')
+      }
 
-//       req.user = verifiedToken // role , id
+      req.user = verifiedToken // role , id
 
-//       // role diye guard korar jnno
-//       if (requiredRoles.length && !requiredRoles.includes(verifiedToken.role)) {
-//         throw new ApiError(403, 'Forbidden role ')
-//       }
+      // role diye guard korar jnno
+      if (requiredRoles.length && !requiredRoles.includes(verifiedToken.role)) {
+        throw new ApiError(403, 'Forbidden role ')
+      }
 
-//       next()
-//     } catch (error) {
-//       next(error)
-//     }
-//   }
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
 
-// export default auth
+export default auth
