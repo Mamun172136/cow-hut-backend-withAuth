f0ffd2f1214dddffdfc09cda09242bca554509be
Mamun172136 <ma1536204@gmail.com>
2023-07-04 14:05:55 +0600

    add  order

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -1,26 +1,26 @@
-// import { Model, Schema, model } from 'mongoose'
-// import { IOrder } from './order.interface'
+import { Model, Schema, model } from 'mongoose'
+import { IOrder } from './order.interface'
 
-// // Create a new Model type that knows about IUserMethods...
-// type OrderModel = Model<IOrder, object>
+// Create a new Model type that knows about IUserMethods...
+type OrderModel = Model<IOrder, object>
 
-// // 2. Create a Schema corresponding to the document interface.
-// const orderSchema = new Schema<IOrder>(
-//   {
-//     buyer: {
-//       type: Schema.Types.ObjectId,
-//       ref: 'User',
-//       required: true,
-//     },
-//     cow: {
-//       type: Schema.Types.ObjectId,
-//       ref: 'Cow',
-//       required: true,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// )
+// 2. Create a Schema corresponding to the document interface.
+const orderSchema = new Schema<IOrder>(
+  {
+    buyer: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    cow: {
+      type: Schema.Types.ObjectId,
+      ref: 'Cow',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-// export const Order = model<IOrder, OrderModel>('Order', orderSchema)
+export const Order = model<IOrder, OrderModel>('Order', orderSchema)
